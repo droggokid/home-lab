9a8f39de8639706420ff44aaa800ce7ec769df07
Radu-Florin Munteanu <raduflorinmunteanu@gmail.com>
2026-03-08 18:33:58 +0100

    new namespace

--- cluster/infra/scripts/postgres-down.sh
+++ cluster/infra/scripts/postgres-down.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Scale postgres deployment down (to 0 replicas)
-kubectl -n apps scale deploy/postgres --replicas=0
-kubectl -n apps wait --for=delete pod -l app=postgres --timeout=60s
+kubectl -n homelab-dev scale deploy/postgres --replicas=0
+kubectl -n homelab-dev wait --for=delete pod -l app=postgres --timeout=60s
--- cluster/infra/scripts/postgres-up.sh
+++ cluster/infra/scripts/postgres-up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Scale postgres deployment up (to 1 replica)
-kubectl -n apps scale deploy/postgres --replicas=1
-kubectl -n apps rollout status deploy/postgres
+kubectl -n homelab-dev scale deploy/postgres --replicas=1
+kubectl -n homelab-dev rollout status deploy/postgres
